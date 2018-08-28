47d1dda724e6f3b1e352d21f4c088eb7f873aa68
yanghui <342@tontisa.com>
2018-08-28 20:42:33 +0800

    git test 2018-08-28

---
+++ GitPro1/GitPro1/src/njupt/GitTest.java
@@ -0,0 +1,12 @@
+package njupt;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("This is  a git test");
+
+	}
+
+}
